7d3747c2522ca03ed752a1efc23bc75c838c6d4f
Chebawca21 <quba200@o2.pl>
2023-01-26 23:05:25 +0100

    organize project

--- src/train.py
+++ src/train.py
@@ -2,8 +2,8 @@ from simulated_annealing import SimulatedAnnealing
 from data_loader import DataLoader
 
 if __name__ == '__main__':
-    dl = DataLoader("data/FHCPCS/graph32.hcp", "data/FHCPCS_sols/graph32.hcp.tou")
-    sa = SimulatedAnnealing(dl.getAdjencyMatrix(), dl.getRoute(), dl.getDim(), K=1400000, t0=0.3, tMin=0.135, alpha=0.9999985, draw=False, print=True)
+    dl = DataLoader("data/FHCPCS/graph1.hcp", "data/FHCPCS_sols/graph1.hcp.tou")
+    sa = SimulatedAnnealing(dl.getAdjencyMatrix(), dl.getRoute(), dl.getDim(), K=100000, t0=0.3, tMin=0.16, alpha=0.999995, draw=False, print=True)
     br, bs, et = sa.anneal()
     print("Finished")
     print("Best Score: ", bs)
